b329a9ee6d2d1d6337a1c2b0aed43e5b9b47a94c
dpeilitz <dpeilitz@kth.se>
2022-04-29 15:01:16 +0200

    a start to adding pygame necessities

---
+++ Rough.py
@@ -0,0 +1,63 @@
+import pygame
+BLACK = (0, 0, 0)
+WHITE = (255,255,255)
+
+FPS = 60
+
+#PAC_MAN = pygame.image.load(os.path.join('Assets', 'pacman.png')) TODO
+#BLUE_GHOST = pygame.image.load(os.path.join('Assets', 'blueghost.png')) TODO
+#RED_GHOST = pygame.image.load(os.path.join('Assets', 'redghost.png')) TODO
+#ORANGE_GHOST = pygame.image.load(os.path.join('Assets', 'orangeghost.png')) TODO
+#PINK_GHOST = pygame.image.load(os.path.join('Assets', 'pinkghost.png')) TODO
+
+#Pygame stuff
+pygame.init()
+window_size = [1000, 1000]
+scr = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Grid")
+
+#Gameboard representation 13x13
+gameboard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+def draw_window():
+    scr.fill(WHITE)
+    pygame.draw.rect()
+    pygame.display.update()
+
+def main():
+    red = pygame.Rect(10,150, 50, 50)
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            #If user quits
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window(red)
+        red.x += 1
+    pygame.quit
+
+if __name__ == "__main__":
+    main()
