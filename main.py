004709b5a8669a4e5e005a62dd246491c3c21424
dpeilitz <dpeilitz@kth.se>
2022-05-04 14:31:43 +0200

    small movement progress

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from constants import *
 from player import Player
+vec = pygame.math.Vector2
+
 
 # PAC_MAN = pygame.image.load(os.path.join('Assets', 'pacman.png')) TODO
 # BLUE_GHOST = pygame.image.load(os.path.join('Assets', 'blueghost.png')) TODO
 # RED_GHOST = pygame.image.load(os.path.join('Assets', 'redghost.png')) TODO
 # ORANGE_GHOST = pygame.image.load(os.path.join('Assets', 'orangeghost.png')) TODO
 # PINK_GHOST = pygame.image.load(os.path.join('Assets', 'pinkghost.png')) TODO
-
 
 class Pacman(object):
     def __init__(self):
@@ -17,8 +18,8 @@ class Pacman(object):
         self.clock = pygame.time.Clock()
         self.running = True
         self.grid  = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -47,15 +48,15 @@ class Pacman(object):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             
         ]
-        self.player = Player(self.grid)
+        self.player = Player(self.grid, self.init_pacman())
 
     def run(self):
         while self.running:
@@ -70,10 +71,18 @@ class Pacman(object):
 
     def startGame(self):
         self.setBackground()
+    
+    def init_pacman(self):
+        for x in range(COLS):
+            for y in range(ROWS):
+                if self.grid[y][x] == 2:
+                    return vec(y, x)
+                
 
     def update(self):
         self.clock.tick(FPS)
         self.player.update()
+        self.grid = self.player.grid
         self.checkEvents()
         self.render()
 
@@ -96,7 +105,10 @@ class Pacman(object):
                                 TILELENGTH, TILELENGTH), 1)
                 if self.grid[y][x] == 1:
                     pygame.draw.rect(self.screen, BLUE, (x*TILELENGTH, y*TILELENGTH,
-                                TILELENGTH, TILELENGTH))   
+                                TILELENGTH, TILELENGTH))
+                if self.grid[y][x] == 2:
+                    pygame.draw.rect(self.screen, RED, (x*TILELENGTH, y*TILELENGTH,
+                                TILELENGTH, TILELENGTH), 1)  
 
 
 
--- player.py
+++ player.py
@@ -2,12 +2,14 @@ import pygame
 from constants import *
 vec = pygame.math.Vector2
 
-
 class Player:
-    def __init__(self, grid):
+    def __init__(self, grid, pos):
         self.grid = grid
-        self.position = vec(200, 400) 
-        self.grid_position = self.position // TILELENGTH
+        self.grid_position = pos
+        self.prev_pos = pos
+        self.pxl_pos = vec(self.grid_position.y * TILELENGTH, self.grid_position.x * TILELENGTH + TILELENGTH / 2)
+        self.pxl_pos.x += TILELENGTH/2
+        print(self.pxl_pos)
         self.direction = vec(0, 1)
         self.speed = 1
         self.radius = 8
@@ -18,13 +20,14 @@ class Player:
         # Check collisions
         #if self.collision() == true:
 
-        self.position += self.direction*self.speed
-        self.grid_position = self.position // TILELENGTH
-        direction = self.get_keypress()
-        if direction != vec(0, 0):
-            self.direction = direction
+        self.pxl_pos += self.direction*self.speed
+        self.grid_position = vec((self.pxl_pos.x - TILELENGTH / 2) // TILELENGTH , (self.pxl_pos.y - TILELENGTH / 2) // TILELENGTH)
+        print(self.grid_position)
+        direction = self.get_keypress(self.direction)
+        self.direction = direction
+        self.move()
 
-    def get_keypress(self):
+    def get_keypress(self, prev_dir):
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_UP]:
             return vec(0, -1)
@@ -34,12 +37,22 @@ class Player:
             return vec(-1, 0)
         elif key_pressed[pygame.K_RIGHT]:
             return vec(1, 0)
-        else:
+        else: 
             return vec(0, 0)
+            #return prev_dir
+
     def render(self, screen):
-        p = self.position
+        p = self.pxl_pos
         pygame.draw.circle(screen, self.color, p, self.radius)
         self.collision()
+
+    def move(self):
+        if self.prev_pos != self.grid_position:
+            xval = int(self.prev_pos.x)
+            yval = int(self.prev_pos.y)
+            self.grid[yval][xval] = 0
+            self.grid[int(self.grid_position.y)][int(self.grid_position.x)] = 2
+            self.prev_pos = self.grid_position
 
     #check the tile pacman is currently on is a wall, returns false otherwise
     def collision(self):
@@ -47,6 +60,7 @@ class Player:
          yval = int(self.grid_position.y + self.direction.y)
          if self.grid[yval][xval] == 1:
             print("HEUREKA")
+            self.direction = vec(0, 0)
             return True
          else:
             return False
