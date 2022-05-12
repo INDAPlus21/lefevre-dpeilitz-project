2f1e8933bbd27c749263550bfaf2a741de52f928
dpeilitz <dpeilitz@kth.se>
2022-05-12 18:37:14 +0200

    Fixed collisions, broke position tracking

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
         self.grid = grid
         self.grid_position = pos
         self.prev_pos = pos
-        self.pxl_pos = vec(self.grid_position.y * TILELENGTH, self.grid_position.x * TILELENGTH)
+        self.pxl_pos = vec(self.grid_position.y * TILELENGTH + TILELENGTH/2, self.grid_position.x * TILELENGTH + TILELENGTH/2 )
         print(self.pxl_pos / TILELENGTH)
         self.direction = vec(-1, 0)
         self.speed = 1
@@ -17,12 +17,22 @@ class Player:
         self.score = 0
 
     def update(self):
-        self.pxl_pos += self.direction*self.speed    
-        if self.direction == (0,1) or self.direction == (1,0):
-            self.grid_position = vec((self.pxl_pos.x) // TILELENGTH  , (self.pxl_pos.y) // TILELENGTH)
+        self.pxl_pos += self.direction*self.speed
+        
+        # if self.direction == (0,1) or self.direction == (1,0):
+        #     self.grid_position = vec((self.pxl_pos.x) // TILELENGTH  , (self.pxl_pos.y) // TILELENGTH)
+        #     #self.grid_position = vec((self.pxl_pos.x + TILELENGTH / 2) // TILELENGTH  , (self.pxl_pos.y + TILELENGTH / 2) // TILELENGTH)
 
-        if self.direction == (0,-1) or self.direction == (-1,0):
-            self.grid_position = vec((self.pxl_pos.x+ TILELENGTH / 2) // TILELENGTH  , (self.pxl_pos.y + TILELENGTH / 2) // TILELENGTH)
+        # if self.direction == (0,-1) or self.direction == (-1,0):
+        #     #self.grid_position = vec((self.pxl_pos.x +  TILELENGTH / 2) // TILELENGTH  , (self.pxl_pos.y + TILELENGTH / 2) // TILELENGTH)
+        #     self.grid_position = vec((self.pxl_pos.x) // TILELENGTH  , (self.pxl_pos.y) // TILELENGTH)
+
+        if (self.pxl_pos.x / TILELENGTH - 0.5).is_integer():
+            self.grid_position.x = self.pxl_pos.x / TILELENGTH
+
+        if (self.pxl_pos.y / TILELENGTH - 0.5).is_integer():
+            self.grid_position.y = (self.pxl_pos.y / TILELENGTH)
+
         self.move_grid()
         #print(self.pxl_pos / TILELENGTH)
 
@@ -56,14 +66,15 @@ class Player:
             return vec(1, 0)
         else: 
             return prev_dir
+            #return vec (0,0)
 
     def render(self, screen):
-        p = (self.pxl_pos.x + TILELENGTH / 2, self.pxl_pos.y + TILELENGTH / 2)
+        p = (self.pxl_pos.x , self.pxl_pos.y)
         pygame.draw.circle(screen, self.color, p, self.radius)
 
     def move_grid(self):
         if self.prev_pos != self.grid_position:
-            
+            print("wat")
             xval = int(self.prev_pos.x)
             yval = int(self.prev_pos.y)
 
@@ -81,9 +92,12 @@ class Player:
 
     #check the tile pacman is currently on is a wall, returns false otherwise
     def collision(self, dir):
-         xval = int(self.grid_position.x + dir.x)
-         yval = int(self.grid_position.y + dir.y)
-         if self.grid[yval][xval] == 1:
+         xval = int((self.grid_position.x + dir.x)* TILELENGTH) 
+         yval = int((self.grid_position.y + dir.y) * TILELENGTH)
+         x_wall = int((self.grid_position.x + dir.x)) 
+         y_wall = int((self.grid_position.y + dir.y))
+         
+         if self.grid[y_wall][x_wall] == 1:
 
             return True
          else:
